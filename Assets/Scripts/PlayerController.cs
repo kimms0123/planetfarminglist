ffca585201862098247757531187c6010004256d
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2026-05-13 00:29:10 +0900

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,17 @@ public class PlayerController : MonoBehaviour
 {
     [Header("이동 설정")]
     public float moveSpeed = 5f;
-
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 lastMoveDir = Vector2.down; // 기본 방향 Down
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void OnMove(InputValue value)
@@ -24,16 +28,27 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = moveInput * moveSpeed;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (moveInput != Vector2.zero)
+        {
+            lastMoveDir = moveInput;
+            animator.SetFloat("MoveX", moveInput.x);
+            animator.SetFloat("MoveY", moveInput.y);
+            animator.SetBool("IsMoving", true);
 
+            // 좌우 반전 (IdleSide 용)
+            if (moveInput.x < 0)
+                spriteRenderer.flipX = true;
+            else if (moveInput.x > 0)
+                spriteRenderer.flipX = false;
+        }
+        else
+        {
+            // 멈췄을 때 마지막 방향 유지
+            animator.SetFloat("MoveX", lastMoveDir.x);
+            animator.SetFloat("MoveY", lastMoveDir.y);
+            animator.SetBool("IsMoving", false);
+        }
+    }
 }
