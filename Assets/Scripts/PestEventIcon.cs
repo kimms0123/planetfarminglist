cb12ac4bbb9a2a9826fdd9f42985edff0da767d8
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2026-05-26 01:14:57 +0900

    맵이랑해충수정

--- Assets/Scripts/PestEventIcon.cs
+++ Assets/Scripts/PestEventIcon.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class PestEventIcon : MonoBehaviour
 {
     private Vector3Int cellPos;
 
-    // TODO: 나중에 해충 스프레이 아이템 검사 추가
-    // public bool requireToolCheck = false;
+    // TODO: 나중에 스프라이트로 교체
+    private TextMeshPro tmp;
+
+    void Awake()
+    {
+        tmp = GetComponent<TextMeshPro>();
+        if (tmp != null)
+        {
+            tmp.text = "!";
+            tmp.color = Color.red;
+            tmp.fontSize = 5;
+            tmp.alignment = TextAlignmentOptions.Center;
+        }
+    }
 
     public void Init(Vector3Int pos)
     {
